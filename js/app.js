0a3573363102d0bcb04cf7ada13b169e89a9a297
Dave Strus <dave@getfretless.com>
2016-06-07 10:43:44 -0400

    Make the delete link work, but not display the changes on the page.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 var mutantsUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants';
 
+$(document).on('click', 'a.delete', function(ev) {
+  var li = $(ev.currentTarget).closest('li');
+  var id = li.data('id');
+  deleteMutant(id);
+});
+
 $(function() {
   $.get({
     url: mutantsUrl,
