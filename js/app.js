2fb49e177e32ebe3dcee2f436ceb4d4c75873389
Dave Strus <dave@getfretless.com>
2016-06-07 10:48:35 -0400

    Remove mutants from the page when they're deleted.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var mutantsUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants';
 $(document).on('click', 'a.delete', function(ev) {
   var li = $(ev.currentTarget).closest('li');
   var id = li.data('id');
-  deleteMutant(id);
+  deleteMutant(id, li);
 });
 
 $(function() {
--- js/delete-mutant.js
+++ js/delete-mutant.js
@@ -1,9 +1,11 @@
-function deleteMutant(id) {
+function deleteMutant(id, li) {
   $.ajax({
     url: mutantsUrl + '/' + id,
     method: 'delete',
     success: function() {
-      alert('BUHLEETED');
+      if (li) {
+        li.remove();
+      }
     }
   });
 };
